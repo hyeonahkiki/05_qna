2a2597b7f208d80078c72056b65b0a9d1534f37a
hyeonah <kkongc7@gmail.com>
2019-08-08 11:15:22 +0900

    models선언 & migration

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=100)
+    user = models.CharField(max_length=20)
+    create_at = models.DateTimeField(auto_now_add=True)
+    
